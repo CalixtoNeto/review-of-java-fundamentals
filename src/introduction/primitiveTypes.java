67a3deb7c238fdc48e294e2209a3990a697a392f
Calixto Neto <calixtoneeto@gmail.com>
2022-07-14 15:57:27 -0300

    Primitive data types in Java, default values, and range of the maximum and the minimum value.

---
+++ src/introduction/primitiveTypes.java
@@ -0,0 +1,50 @@
+package introduction;
+
+
+public class primitiveTypes {
+    static int i;
+    static double d;
+    static float f;
+    static char c;
+    static byte b;
+    static short s;
+    static long l;
+    static boolean result;
+
+    public static void main(String[] args) {
+        //int , double , float, char, byte, short, long, boolean
+
+        //Default Values
+        System.out.println("\nDefault Values");
+        System.out.println("Default Value int    :" + i);
+        System.out.println("Default Value double :" + d);
+        System.out.println("Default Value float  :" + f);
+        System.out.println("Default Value char   :" + c);
+        System.out.println("Default Value byte   :" + b);
+        System.out.println("Default Value short  :" + s);
+        System.out.println("Default Value long   :" + l);
+        System.out.println("Default Value boolean:" + result);
+
+        //Min Values
+        System.out.println("\nMin Values");
+        System.out.println("Default Value int    :" + Integer.MIN_VALUE);
+        System.out.println("Default Value double :" + Double.MIN_VALUE);
+        System.out.println("Default Value float  :" + Float.MIN_VALUE);
+        System.out.println("Default Value char   :" + Character.MIN_VALUE);
+        System.out.println("Default Value byte   :" + Byte.MIN_VALUE);
+        System.out.println("Default Value short  :" + Short.MIN_VALUE);
+        System.out.println("Default Value long   :" + Long.MIN_VALUE);
+
+        //Max Values
+        System.out.println("\nMax Values");
+        System.out.println("Default Value int    :" + Integer.MIN_VALUE);
+        System.out.println("Default Value double :" + Double.MIN_VALUE);
+        System.out.println("Default Value float  :" + Float.MIN_VALUE);
+        System.out.println("Default Value char   :" + Character.MIN_VALUE);
+        System.out.println("Default Value byte   :" + Byte.MIN_VALUE);
+        System.out.println("Default Value short  :" + Short.MIN_VALUE);
+        System.out.println("Default Value long   :" + Long.MIN_VALUE);
+
+
+    }
+}
